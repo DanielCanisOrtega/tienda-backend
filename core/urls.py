46c521b5f628f3c33340e91929c9df76af630c4a
Daniel <danielstevenco@ufps.edu.co>
2025-04-01 22:35:21 -0500

    se revierte el registro e inicio de sesion

--- core/urls.py
+++ core/urls.py
@@ -19,6 +19,6 @@ router.register(r'cajas', CajaViewSet, basename='caja')
 urlpatterns = [
     #path('auth/', include('dj_rest_auth.urls')), arreglar
     path('', include(router.urls)),  # Incluir todas las rutas del router
-    path('auth/registration/', include('dj_rest_auth.registration.urls')),
-    path('auth/password-reset-sms/', PasswordResetSMSView.as_view(), name='password-reset-sms'),
+    #path('auth/registration/', include('dj_rest_auth.registration.urls')),
+    #path('auth/password-reset-sms/', PasswordResetSMSView.as_view(), name='password-reset-sms'),
 ]
