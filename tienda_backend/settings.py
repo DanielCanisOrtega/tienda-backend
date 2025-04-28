07f7f355d39322aacc1d966002b2316ddc2e5727
Daniel <danielstevenco@ufps.edu.co>
2025-04-28 09:17:23 -0500

    se corrige error

--- tienda_backend/settings.py
+++ tienda_backend/settings.py
@@ -27,10 +27,10 @@ DEBUG = True
 
 ALLOWED_HOSTS = ['tienda-backend-p9ms.onrender.com','127.0.0.1', 'localhost']
 
-CSRF_TRUSTED_ORIGINS = ['https://tienda-backend-p9ms.onrender.com',"http://localhost:3000"]
+CSRF_TRUSTED_ORIGINS = ['https://tienda-backend-p9ms.onrender.com',"http://localhost:3000","https://tienda-web-seminario.vercel.app/"]
 
 CORS_ALLOWED_ORIGINS = [
-    "https://tienda-web-seminario.vercel.app/", 
+    "https://tienda-web-seminario.vercel.app",
     "http://localhost:3000",  
     "http://192.168.1.37:3000",
 ]
